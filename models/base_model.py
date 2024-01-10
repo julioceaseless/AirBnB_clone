207db945d6a8d3783630ee15e30a8eb3adccc39a
julioceaseless <julioceaseless@gmail.com>
2024-01-10 17:02:46 +0000

    fix pycodestle E501 line too long error

--- models/base_model.py
+++ models/base_model.py
@@ -37,7 +37,8 @@ class BaseModel:
 
     def __str__(self):
         """Return a string representation of the BaseModel instance."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         """Update the 'updated_at' attribute with the current datetime."""
