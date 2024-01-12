7ccb49b3650b19d995896ff92548c78fc4a89dbb
julioceaseless <julioceaseless@gmail.com>
2024-01-12 06:17:22 +0000

    json serializer and deserializer code

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from datetime import datetime
+from models import storage
 import uuid
 
 
@@ -19,6 +20,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def to_dict(self):
         """
@@ -39,3 +41,4 @@ class BaseModel:
     def save(self):
         """Update the 'updated_at' attribute with the current datetime."""
         self.updated_at = datetime.now()
+        storage.save()
